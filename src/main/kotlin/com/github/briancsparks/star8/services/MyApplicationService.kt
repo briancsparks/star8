5808fcbbcc152ece58a408c26c5688ed83decaca
GitHub Action <action@github.com>
2021-12-04 03:25:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/briancsparks/star8/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/briancsparks/star8/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.briancsparks.star8.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.briancsparks.star8.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/briancsparks/star8/MyPluginTest.kt
+++ src/test/kotlin/com/github/briancsparks/star8/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.briancsparks.star8
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
